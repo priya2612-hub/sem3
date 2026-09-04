1f8e64bf152e945f5fccfce0229768f1079067b2
priya2612-hub <priyaramamoorthy2612@gmail.com>
2026-09-04 22:41:15 +0530

    Added Q4 First Non-Repeating Character

---
+++ Week1/Q4_FirstNonRepeating.java
@@ -0,0 +1,9 @@
+import java.util.*;
+
+public class Q4_FirstNonRepeating {
+
+    static char findFirstNonRepeatingChar(String text) {
+
+        HashMap<Character, Integer> frequency = new HashMap<>();
+
+
