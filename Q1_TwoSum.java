8151d8f87320c9722957c77cccef2adc61ddcc23
priya2612-hub <priyaramamoorthy2612@gmail.com>
2026-09-05 14:55:16 +0530

    Add Q1 Two Sum

---
+++ Q1_TwoSum.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class Q1_TwoSum {
+
+    static int[] twoSum(int[] nums, int target) {
+
+        for (int i = 0; i < nums.length; i++) {
+
+            for (int j = i + 1; j < nums.length; j++) {
+
+                if (nums[i] + nums[j] == target) {
+                    return new int[]{i, j};
+                }
+            }
+        }
+
+        return new int[]{-1, -1};
+    }
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter number of elements: ");
+        int n = sc.nextInt();
+
+        int[] nums = new int[n];
+
