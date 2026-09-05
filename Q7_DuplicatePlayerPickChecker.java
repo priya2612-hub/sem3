b2cac9b423fb016ebb90dd191996f3b50db3c73b
priya2612-hub <priyaramamoorthy2612@gmail.com>
2026-09-05 15:19:09 +0530

    Add Q7 Duplicate Player Pick Checker

---
+++ Q7_DuplicatePlayerPickChecker.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Q7_DuplicatePlayerPickChecker {
+
+    static String findDuplicatePick(String[] playerNames) {
+
+        for (int i = 0; i < playerNames.length; i++) {
+            for (int j = i + 1; j < playerNames.length; j++) {
+
+                if (playerNames[i].equals(playerNames[j])) {
+                    return "Duplicate Found: " + playerNames[i];
+                }
+            }
+
