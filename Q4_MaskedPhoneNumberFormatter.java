c9c81c753b7bfd3e25cb1e79841c89f5cc4afcb2
priya2612-hub <priyaramamoorthy2612@gmail.com>
2026-09-05 14:39:15 +0530

    Add Q4 Masked Phone Number Formatter

---
+++ Q4_MaskedPhoneNumberFormatter.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Q4_MaskedPhoneNumberFormatter {
+
+    static String maskPhoneNumber(String phone) {
+
+        if (phone.length() != 10) {
+            return "Invalid phone number";
+        }
+
+        for (int i = 0; i < phone.length(); i++) {
+            if (!Character.isDigit(phone.charAt(i))) {
+                return "Invalid phone number";
+            }
+
+
