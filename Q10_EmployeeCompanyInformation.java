2cb191b3fc8b227c0b19e84e89f17ffa6f8c82b8
priya2612-hub <priyaramamoorthy2612@gmail.com>
2026-09-18 22:21:06 +0530

    Add Q10 Employee Company Information

---
+++ Q10_EmployeeCompanyInformation.java
@@ -0,0 +1,32 @@
+class Q10_EmployeeCompanyInformation {
+
+    static class Employee {
+        String empName;
+        double salary;
+
+        static String companyName = "Bright Horizon Technologies";
+        static int employeeCount = 0;
+
+        Employee(String empName, double salary) {
+            this.empName = empName;
+            this.salary = salary;
+            employeeCount++;
+        }
+
+        static void printCompanyInfo() {
+            System.out.println(companyName);
+            System.out.println("Employees on record: " + employeeCount);
+        }
+    }
+
+    public static void main(String[] args) {
+
+        Employee e1 = new Employee("Priya", 50000);
+        Employee e2 = new Employee("Arjun", 45000);
+        Employee e3 = new Employee("Divya", 60000);
+
+        System.out.println("3 Employee objects created");
+
+        Employee.printCompanyInfo();
+    }
+}
