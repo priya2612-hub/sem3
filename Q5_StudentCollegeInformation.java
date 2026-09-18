3ee7e0da155ca6de2253cd8f2a07d411e13890c8
priya2612-hub <priyaramamoorthy2612@gmail.com>
2026-09-18 22:14:16 +0530

    Add Q5 Student College Information

---
+++ Q5_StudentCollegeInformation.java
@@ -0,0 +1,33 @@
+class Q5_StudentCollegeInformation {
+
+    static class Student {
+        String name;
+        int attendance;
+
+        static String collegeName =
+            "SRM Institute of Science and Technology";
+
+        static int studentCount = 0;
+
+        Student(String name, int attendance) {
+            this.name = name;
+            this.attendance = attendance;
+            studentCount++;
+        }
+
+        static void printCollegeInfo() {
+            System.out.println(collegeName);
+            System.out.println("Students created: " + studentCount);
+        }
+    }
+
+    public static void main(String[] args) {
+
+        Student s1 = new Student("Ravi", 90);
+        Student s2 = new Student("Anitha", 85);
+
+        System.out.println("2 Student objects created");
+
+        Student.printCollegeInfo();
+    }
+}
