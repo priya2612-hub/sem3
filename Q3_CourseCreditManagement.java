45321911f066d57da7d8786506073c2981765291
priya2612-hub <priyaramamoorthy2612@gmail.com>
2026-09-18 22:12:07 +0530

    Add Q3 Course Credit Management

---
+++ Q3_CourseCreditManagement.java
@@ -0,0 +1,39 @@
+class Q3_CourseCreditManagement {
+
+    static class Course {
+        String code;
+        String title;
+        int credits;
+        int labCredits;
+
+        Course(String code, String title, int credits, int labCredits) {
+            this.code = code;
+            this.title = title;
+            this.credits = credits;
+            this.labCredits = labCredits;
+        }
+
+        Course(String code, String title, int credits) {
+            this(code, title, credits, 0);
+        }
+
+        int totalCredits() {
+            return credits + labCredits;
+        }
+    }
+
+    public static void main(String[] args) {
+
+        Course theory = new Course("21CSC201J", "Data Structures", 4);
+
+        Course lab = new Course("21CSC205L", "DSA Lab", 3, 1);
+
+        System.out.println(
+            theory.code + " total credits: " + theory.totalCredits()
+        );
+
+        System.out.println(
+            lab.code + " total credits: " + lab.totalCredits()
+        );
+    }
+}
