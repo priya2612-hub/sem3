df9d2457b9851c5cff930da0f4aead1aacdbeb4e
priya2612-hub <priyaramamoorthy2612@gmail.com>
2026-09-25 21:33:59 +0530

    Add Q5 Shopping Cart

---
+++ Q5_ShoppingCart.java
@@ -0,0 +1,53 @@
+class Cart {
+    private double[] prices;
+    private int itemCount;
+    private final String cartId;
+
+    Cart(String cartId, int maxItems) {
+        this.cartId = cartId;
+        this.prices = new double[maxItems];
+        this.itemCount = 0;
+    }
+
+    public void addItem(double price) {
+        if (itemCount < prices.length) {
+            prices[itemCount] = price;
+            itemCount++;
+        } else {
+            System.out.println("Cart is full.");
+        }
+    }
+
+    public double getTotal() {
+        double total = 0;
+
+        for (int i = 0; i < itemCount; i++) {
+            total = total + prices[i];
+        }
+
+        return total;
+    }
+
+    public int getItemCount() {
+        return itemCount;
+    }
+
+    public String getCartId() {
+        return cartId;
+    }
+}
+
+public class Q5_ShoppingCart {
+    public static void main(String[] args) {
+
+        Cart cart = new Cart("CART-5", 20);
+
+        cart.addItem(250);
+        cart.addItem(99);
+        cart.addItem(151);
+
+        System.out.println("Cart ID: " + cart.getCartId());
+        System.out.println("Total: " + cart.getTotal());
+        System.out.println("Item count: " + cart.getItemCount());
+    }
+}
